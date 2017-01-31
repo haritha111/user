aecab37ae6207c7339f3f722567e3b0d8468d4ef
haritha111 <haritha0823@gmail.com>
2017-01-31 13:00:24 -0800

    Add files via upload

---
+++ UserController.java
@@ -0,0 +1,36 @@
+﻿package com.logilync.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.logilync.domain.User;
+import com.logilync.repository.UserRepo;
+
+@Controller
+public class UserController {
+	@Autowired
+	UserRepo userRepo;
+	
+	
+	@RequestMapping(value="/createUser", method=RequestMethod.GET)
+	public String showCreateUser(){
+		return "createUser";
+	}
+	
+	@RequestMapping(value="/saveUser", method=RequestMethod.POST)
+	public String saveCreateUser(User user, Model model){
+		user=userRepo.save(user);
+                List<User> users=userRepo.findAll();
+                for(User u : users){
+                if(user.containsKey(u.getEmail())){
+                else
+                {
+		model.addAttribute("user",user);
+		}
+                return "user";
+	}
+
+}
---
+++ UserRepo.java
@@ -0,0 +1,9 @@
+﻿package com.logilync.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.logilync.domain.Person;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
